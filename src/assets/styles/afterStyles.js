a8d04bb9c503f885933b4079515278e97dcfcbc3
Marty Wernimont <mwernimont87@gmail.com>
2020-04-09 11:59:52 -0500

    Basic Context Slider Functionality

---
+++ src/assets/styles/afterStyles.js
@@ -0,0 +1,129 @@
+export default {
+    style: {
+        version: 8,
+        sources: {
+            basemap: {
+                type: 'vector',
+                'tiles': ['https://maptiles-prod-website.s3-us-west-2.amazonaws.com/misctilesets/usstatecounties/{z}/{x}/{y}.pbf'],
+                'minzoom': 2, // setting this to equal the minzoom of main map, real tile extent is 2
+                'maxzoom': 6  // setting this to equal the maxzoom of main map, real tile extent is 10
+            },
+            nhd_streams_grouped: {
+                type: 'vector',
+                'tiles': ['https://maptiles-prod-website.s3-us-west-2.amazonaws.com/nhdstreams_grouped/{z}/{x}/{y}.pbf'],
+                'minzoom': 2, // setting this to equal the minzoom of main map, real tile extent is 0
+                'maxzoom': 6  // setting this to equal the maxzoom of main map, real tile extent is 10
+            },
+            openmaptiles: {
+                type: 'vector',
+                'tiles': ['https://maptiles-prod-website.s3-us-west-2.amazonaws.com/openmaptiles/baselayers/{z}/{x}/{y}.pbf'],
+                'minzoom': 2,
+                'maxzoom': 6
+            },
+            hillshade: {
+                type: 'raster',
+                'tiles': ['https://maptiles-prod-website.s3-us-west-2.amazonaws.com/openmaptiles/omthillshade/{z}/{x}/{y}.png'],
+                'minzoom': 2,
+                'maxzoom': 12,
+                'tileSize': 256
+            }
+        },
+        'sprite': '',
+        'glyphs': 'https://orangemug.github.io/font-glyphs/glyphs/{fontstack}/{range}.pbf',
+        'layers': [
+            {
+                'id': 'background',
+                'paint': {
+                    'background-color': 'hsl(47, 26%, 88%)'
+                },
+                'type': 'background',
+                'showButtonLayerToggle': false
+            },
+            {
+                'id': 'Neighboring Countries',
+                'type': 'fill',
+                'source': 'basemap',
+                'minzoom': 2,
+                'maxzoom': 24,
+                'source-layer': 'neighboringcountry',
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'paint': {
+                    'fill-color': 'hsl(47, 26%, 88%)'
+                },
+                'showButtonLayerToggle': false
+            },
+            {
+                'filter': ['all', ['==', '$type', 'Polygon'],
+                    ['!=', 'intermittent', 1]
+                ],
+                'id': 'water',
+                'paint': {
+                    'fill-color': 'hsl(205, 56%, 73%)'
+                },
+                'source': 'openmaptiles',
+                'source-layer': 'water',
+                'type': 'fill',
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'showButtonLayerToggle': false
+            },
+            {
+                'id': 'Terrain',
+                'type': 'raster',
+                'source': 'hillshade',
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'showButtonLayerToggle': true,
+                'showButtonStreamToggle': false,
+            },
+            {
+                'id': 'states',
+                'type': 'line',
+                'source': 'basemap',
+                'source-layer': 'states',
+                'minzoom': 2,
+                'maxzoom': 24,
+                'layout': {
+                    'visibility': 'visible',
+                },
+                'paint': {
+                    'line-color': 'rgb(0,0,0)'
+                }
+
+            },
+            {
+                'filter': ['all', ['==', '$type', 'Point'],
+                    ['==', 'class', 'city']
+                ],
+                'id': 'place_label_city',
+                'layout': {
+                    'text-field': '{name:latin}\n{name:nonlatin}',
+                    'text-font': ['Noto Sans Regular'],
+                    'text-max-width': 10,
+                    'text-size': {
+                        'stops': [
+                            [3, 12],
+                            [8, 16]
+                        ]
+                    }
+                },
+                'maxzoom': 16,
+                'minzoom': 5,
+                'paint': {
+                    'text-color': 'hsl(0, 0%, 0%)',
+                    'text-halo-blur': 0,
+                    'text-halo-color': 'hsla(0, 0%, 100%, 0.75)',
+                    'text-halo-width': 2
+                },
+                'source': 'openmaptiles',
+                'source-layer': 'place',
+                'type': 'symbol',
+                'showButtonLayerToggle': false
+            }
+        ]
+    }
+}
---
+++ src/components/MapboxSlider.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    id="mapboxslider"
+    class="section"
+  >
+    <h2>Context Slider</h2>
+    <div id="maps">
+      <div id="comparison-container">
+        <div
+          id="before"
+          class="map"
+        />
+        <div
+          id="after"
+          class="map"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import beforeStyle from '../assets/styles/beforeStyles';
+import afterStyle from '../assets/styles/afterStyles';
+import mapboxgl from 'mapbox-gl';
+import MapboxCompare from 'mapbox-gl-compare';
+export default {
+    name: 'MapboxSlider',
+    mounted(){
+        this.CreateMaps();
+    },
+    methods: {
+        CreateMaps(){
+            let beforeMap = new mapboxgl.Map({
+                container: 'before',
+                style: beforeStyle.style,
+                center: [-95.7129, 37.0902],
+                zoom: 2
+            });
+            let afterMap = new mapboxgl.Map({
+                container: 'after',
+                style: afterStyle.style,
+                center: [-95.7129, 37.0902],
+                zoom: 2
+            });
+            let container = '#comparison-container';
+
+            new MapboxCompare(beforeMap, afterMap, container);
+        }
+    }
+}
+</script>
+<style scoped lang='scss'>
+@import '~mapbox-gl/dist/mapbox-gl.css';
+@import '~mapbox-gl-compare/dist/mapbox-gl-compare.css';
+#maps{
+    position: relative;
+    height: 300px;
+    overflow: hidden;
+}
+.map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+}
+</style>
