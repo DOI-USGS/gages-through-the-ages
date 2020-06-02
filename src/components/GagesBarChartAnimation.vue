9d5a753cdc9d9240ddfecab9a089aacdf014410b
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-06-02 11:31:50 -0500

    more gagesbarchart text to json

--- src/components/GagesBarChartAnimation.vue
+++ src/components/GagesBarChartAnimation.vue
@@ -4,9 +4,9 @@
     class="section"
   >
     <div class="container">
-      <h2>Stream Gages Through the Ages</h2>
+      <h2>{{ text.textContents.title }}</h2>
 
-      <h3>U.S. Geological Survey Active Stream Gages</h3>
+      <h3>{{ text.textContents.introText }}</h3>
     </div>
     <figure>
       <svg
@@ -2637,26 +2637,18 @@
         </g>
       </svg>
     </figure>
-    <p>
-      The <a
-        href="https://www.usgs.gov/science/mission-areas/water/groundwater-and-streamflow-information?qt-programs_l2_landing_page=0#qt-programs_l2_landing_page"
-      >U.S.
-        Geological Survey Groundwater and Streamflow Information Program</a>
-      is funded by Congress to monitor drought and flood across the US through a network of stream gages.
-    </p>
-
-    <p>
-      Why should you care about USGS streamgages? The USGS has been monitoring streamflow since 1889, and data from
-      streamgages
-      have since become critical to hydropower operations, recreation, streamflow prediction, flood forecasts, emergency
-      management, engineering designs, and many other purposes.
-    </p>
+    <div
+      v-for="paragraph in text.textContents.paragraphSections"
+      :key="paragraph.paragraphText"
+    >
+      <p><span v-html="paragraph.paragraphText" /></p>
+    </div>
   </div>
 </template>
 <script>
     import $ from 'jquery';
     import yearData from "../assets/data/year-data.js"
-
+    import gagesBarChartAnimationText from "../assets/gagesBarChartAnimation/gagesBarChartAnimationText";
     export default {
         name: 'GagesBarChartAnimation',
         data() {
@@ -2675,7 +2667,8 @@
                 svg: null,
                 svgId: null,
                 tooltipGroupId: null,
-                TOOLTIP_HTML: null
+                TOOLTIP_HTML: null,
+                text: gagesBarChartAnimationText
 
             }
         },
