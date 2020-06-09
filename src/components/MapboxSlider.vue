999b16e922c8ad2664d2f6a757718d691ba5f796
Marty Wernimont <mwernimont87@gmail.com>
2020-06-09 12:30:36 -0500

    1970 Urban Extents

--- src/components/MapboxSlider.vue
+++ src/components/MapboxSlider.vue
@@ -66,7 +66,7 @@ export default {
             georgiaCenter: [-84.3880, 33.7490],
             coloradoCenter: [-105.7821, 39.5501],
             zoom: 6,
-            interactive: false,
+            interactive: true,
             atlantaText: atlantaSliderText.textContents,
             coloradoText: coloradoSliderText.textContents
         }
@@ -76,6 +76,10 @@ export default {
     },
     methods: {
         createMaps() {
+            const co1970Extent = 'https://maptiles-prod-website.s3-us-west-2.amazonaws.com/gagesthroughages/urbanExtents/urban_areas_co_1970/{z}/{x}/{y}.pbf';
+            const co2018Extent = 'https://maptiles-prod-website.s3-us-west-2.amazonaws.com/gagesthroughages/urbanExtents/urban_areas_co_2018/{z}/{x}/{y}.pbf';
+            const ga1970Extent = 'https://maptiles-prod-website.s3-us-west-2.amazonaws.com/gagesthroughages/urbanExtents/urban_areas_ga_1970/{z}/{x}/{y}.pbf';
+            const ga2018Extent = 'https://maptiles-prod-website.s3-us-west-2.amazonaws.com/gagesthroughages/urbanExtents/urban_areas_ga_2018/{z}/{x}/{y}.pbf';
             let radius = 4;
             let urban = '#f7bb2e';
             let rural = '#b087bd';
@@ -125,12 +129,17 @@ export default {
                 maxZoom: 9,
                 interactive: this.interactive
             });
+            //Add Urban Extents
+            this.AddUrbanExtent(coloradoBeforeMap, co1970Extent, 'co1970Extent', 'urban_areas_co_1970');
+            this.AddUrbanExtent(coloradoAfterMap, co2018Extent, 'co2018Extent', 'urban_areas_co_2018');
+            this.AddUrbanExtent(georgiaBeforeMap, ga1970Extent, 'ga1970Extent', 'urban_areas_ga_1970');
+            this.AddUrbanExtent(georgiaAfterMap, ga2018Extent, 'ga2018Extent', 'urban_areas_ga_2018');
             //Get maps canvases
             let georgiaBeforeMapCanvas = georgiaBeforeMap.getCanvasContainer();
             let georgiaAfterMapCanvas = georgiaAfterMap.getCanvasContainer();
             let coloradoBeforeMapCanvas = coloradoBeforeMap.getCanvasContainer();
             let coloradoAfterMapCanvas = coloradoAfterMap.getCanvasContainer();
-            //Creates dives in the canvas area
+            //Creates divs in the canvas area
             this.CreateYearDiv(beforeYear, "beforeYear", georgiaBeforeMapCanvas);
             this.CreateYearDiv(afterYear, "afterYear", georgiaAfterMapCanvas);
             this.CreateYearDiv(beforeYear, "beforeYear", coloradoBeforeMapCanvas);
@@ -143,20 +152,35 @@ export default {
 
             let georgiaContainer = '#georgia-comparison-container';
             let coloradoContainer = '#colorado-comparison-container';
-
+            //Creates the mapbox compares
             new MapboxCompare(georgiaBeforeMap, georgiaAfterMap, georgiaContainer);
             new MapboxCompare(coloradoBeforeMap, coloradoAfterMap, coloradoContainer);
         },
+        AddUrbanExtent(map, url, source, sourceLayer){
+          let self = this;
+          map.on('load', function(){
+            let findSymbolId = self.GetMapLayers(map);
+            console.log(findSymbolId);
+            map.addSource(source, {
+              type: 'vector',
+              tiles: [url]
+            });
+            map.addLayer({
+              'id': source,
+              'source': source,
+              'source-layer': sourceLayer,
+              'type': 'fill',
+              'paint':{
+                'fill-color': 'rgb(208,209,207)'
+              }
+            },
+            findSymbolId)
+          })
+        },
         AddGeoJSON(map, data, source, radius, urban, rural){
+          let self = this;
           map.on('load', function() {
-              var layers = map.getStyle().layers;
-              let symbolId;
-              for(let i = 0; i < layers.length; i++){
-                if(layers[i].type === 'symbol'){
-                    symbolId = layers[i].id;
-                    break;
-                  }
-                }
+              let findSymbolId = self.GetMapLayers(map);
               map.addSource(source, {
                   type: 'geojson',
                   data: data
@@ -185,9 +209,20 @@ export default {
                   },
                   'filter': ['==', '$type', 'Point']
               },
-              symbolId
+              findSymbolId
               );
           });
+        },
+        GetMapLayers(map){
+          var layers = map.getStyle().layers;
+          let symbolId;
+          for(let i = 0; i < layers.length; i++){
+            if(layers[i].type === 'symbol'){
+              symbolId = layers[i].id;
+              break;
+            }
+          }
+          return symbolId;
         },
         CreateYearDiv(year, divId, canvas){
           let div = document.createElement('div');
