de94d769b0d6614f61921a1d921fe146e1eb251b
Hines <mhines@usgs.gov>
2020-06-11 14:59:52 -0500

    consistency with above mention

--- src/assets/takeAway/takeAwayText.js
+++ src/assets/takeAway/takeAwayText.js
@@ -3,7 +3,7 @@ export default {
         title: "Take Away",
         introText: "",
         paragraphSections: [
-            {paragraphText: "USGS responds to the present-day needs of water management and the protection of life and property. As factors change that influence where water measurements are needed, USGS continues to adapt. A new initiative established in 2019, called the “Next-Gen Water Observing System” is piloting new advanced water monitoring in the Delaware River Basin and will be expanding into the Upper Colorado River Basin next year. These new sources of water information will inform water resources decisions and help advance the understanding of how best to meet the nation’s water needs."},
+            {paragraphText: "USGS responds to the present-day needs of water management and the protection of life and property. As factors change that influence where water measurements are needed, USGS continues to adapt. A new initiative established in 2019, called the “Next-Generation Water Observing System” is piloting new advanced water monitoring in the Delaware River Basin and will be expanding into the Upper Colorado River Basin next year. These new sources of water information will inform water resources decisions and help advance the understanding of how best to meet the nation’s water needs."},
         ]
     }
 }
