4f6a6deb9bebb6a6e73bdea7b3dee2c3c23e6333
Marty Wernimont <mwernimont87@gmail.com>
2020-06-16 09:52:01 -0500

    Text Update 061620

--- src/assets/gagesBarChartAnimation/gagesBarChartAnimationText.js
+++ src/assets/gagesBarChartAnimation/gagesBarChartAnimationText.js
@@ -4,29 +4,22 @@ export default {
         introText: "A story of hydropower exploration to urban flood monitoring",
         graphTitle: "U.S. Geological Survey’s Active Stream Gages",
         paragraphSections: [
-            {paragraphText: "The U.S. Geological Survey (USGS) Groundwater and Streamflow Information Program" + 
-                            " is funded by Congress to monitor drought, flood, and water quality across the US through a" + 
-                            " network of stream gages. Many of these gages are also supported through cooperation with state" + 
-                            " and local funding partners."
+            {paragraphText: "The U.S. Geological Survey (USGS) Groundwater and Streamflow Information Program is " +  
+                            "funded by Congress to monitor drought, flood, and water quality across the US through a " +
+                            "network of stream gages. Many of these gages are also supported through cooperation with state " + 
+                            "and local funding partners."
             },
-            {paragraphText: "The USGS began using stream gages to quantify the nation's flowing waters in 1889." +  
-                            " These days, many gages are equipped with sensors that measure temperature, conductivity," + 
-                            " salinity, and much more, and each gage sends the data in real time to a National Water Information System." +  
-                            " The data from these gages have become critical to forecasting floods, managing streamflow, and operating" + 
-                            " hydropower plants, as well as recreation, emergency management, engineering designs, and much more. "
+            {paragraphText: "The USGS began using stream gages to quantify the nation's flowing waters in 1889. " + 
+                            "These days, many gages are equipped with sensors that measure temperature, conductivity, " + 
+                            "salinity, and much more, and each gage sends the data in near real-time to a National Water Information System. " +
+                            "The data from these gages have become critical to forecasting floods, managing streamflow, and operating hydropower " + 
+                            "plants, as well as recreation, emergency management, engineering designs, and much more." 
             },
-
-            {paragraphText: "Factors that influence why the USGS monitors particular waterways have changed a lot in the last 120 years." +   
-                            " Gages are built at specific locations for many reasons, and those reasons are reflected in the number and" + 
-                            " location of gages through time.  Their placement tells a story of a changing nation."
+            {paragraphText: "<strong>Since the 1960’s, the number of urban gages has increased and their distribution has shifted.</strong>"
             },
-            {paragraphText: "Up to the 1960’s, the US invested in a wide geographic dispersal of gages, with a specific focus on assessing" + 
-                            " streams for their hydropower potential.  That meant that gages covered a wide geographic area, especially in" +
-                            " non-urban parts of the country."
-            },
-            {paragraphText: "A decreasing demand for hydropower after the 1960’s and 70’s, combined with increasing populations in cities and" +
-                            " the need for urban flood management, meant that the location of new gages began to migrate towards urban areas." + 
-                            " Today, many gages are clustered near urban areas to support flood prediction and advanced evacuation warnings."
+            {paragraphText: "Factors that influence why the USGS monitors particular waterways have changed a lot in the last 130 years. " +
+                            "Gages are built at specific locations for many reasons, and those reasons are reflected in the number and " + 
+                            "location of gages through time.  Their placement tells a story of a changing nation. "
             }
         ]
     }
--- src/assets/takeAway/takeAwayText.js
+++ src/assets/takeAway/takeAwayText.js
@@ -1,9 +1,14 @@
 export default {
     textContents: {
-        title: "Take Away",
+        title: "The Next Generation Water Observing System ",
         introText: "",
         paragraphSections: [
-            {paragraphText: "USGS responds to the present-day needs of water management and the protection of life and property. As factors change that influence where water measurements are needed, USGS continues to adapt. A new initiative established in 2019, called the “Next-Generation Water Observing System” is piloting new advanced water monitoring in the Delaware River Basin and will be expanding into the Upper Colorado River Basin next year. These new sources of water information will inform water resources decisions and help advance the understanding of how best to meet the nation’s water needs."},
+            {paragraphText: 'As factors that influence water monitoring change, the USGS is continually ' + 
+                            'adapting to the present-day needs of water management and the protection of life ' +
+                            'and property. A new initiative established in 2019, called the "Next-Generation Water Observing System” ' +
+                            'is piloting new advanced water monitoring in the Delaware River Basin and will be expanding into the ' +
+                            'Upper Colorado River Basin next year. These new sources of water information will inform water resources ' + 
+                            'decisions and advance the understanding of how best to meet the nation’s changing water needs.'},
         ]
     }
 }
