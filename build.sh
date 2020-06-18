cce6b0861fadd26f5511dfca90eee9a09ebdc235
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-06-18 17:32:59 -0500

    word change

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-echo "this is the var $E_VUE_BUILD_MODE"
+echo "this is the build mode $E_VUE_BUILD_MODE"
 if [ "$E_VUE_BUILD_MODE" = "test" ]
 then npm run build-test
 elif [ "$E_VUE_BUILD_MODE" = "beta" ]
