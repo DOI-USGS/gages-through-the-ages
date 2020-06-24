07a0c807774ded8d42f843bf0dd69b936fe0cb56
Marty Wernimont <mwernimont87@gmail.com>
2020-06-24 10:28:37 -0500

    Updated SVG with 2019 data and text update

--- src/assets/mapboxSlider/atlantaSliderText.js
+++ src/assets/mapboxSlider/atlantaSliderText.js
@@ -15,8 +15,8 @@ export default {
             },
             {belowSliderText: "As the creation of new dams slowed significantly after the 1960’s, some hydropower-oriented gages " +
                             "were no longer essential.  Many of those gages were retired. With that shift, more new gages were " +
-                            "established in urban areas.  A conservative estimate* suggests only 15% of gages would’ve been considered " + 
-                            "urban in 1967, but in 2019, 23% of all active USGS gages were located within urban areas.  Today, many gages " +
+                            "established in urban areas.  A conservative estimate* suggests only 16% of gages would’ve been considered " + 
+                            "urban in 1967, but in 2018, 23% of all active USGS gages were located within urban areas.  Today, many gages " +
                             "are clustered near urban areas to support flood prediction and advanced evacuation warnings."
             }
         ]
