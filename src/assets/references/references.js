9f8a7c5735c4d2e91ad1fce46d88bcf01fc6796c
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-06-25 18:04:19 -0500

    added blank references

---
+++ src/assets/references/references.js
@@ -0,0 +1,25 @@
+// Use this template for adding additional references.
+// {
+//     reference: "",
+//          href: "",
+//         googleAnalytics: "$ga.commands.trackName.bind(this, 'click-reference', 'click', 'user went to reference ')",
+// }
+
+export default {
+    referenceSection: {
+        title: "References",
+        references: [
+            {
+                reference: "Sample reference one",
+                href: "",
+                googleAnalytics: "$ga.commands.trackName.bind(this, 'click-reference', 'click', 'user went to reference')",
+            },
+            {
+                reference: "Sample reference two",
+                href: "",
+                googleAnalytics: "$ga.commands.trackName.bind(this, 'click-reference', 'click', 'user went to reference')",
+            },
+
+        ]
+    }
+}
--- src/components/References.vue
+++ src/components/References.vue
@@ -1,16 +1,36 @@
 <template>
-  <div
-    id="references"
-    class="section"
-  >
-    <h2>References:</h2>
+  <div>
+    <h2>{{ title }}:</h2>
+    <div
+      v-for="reference in references"
+      :key="reference.reference"
+    >
+      <p class="about-ref">
+        {{ reference.reference }}
+        <a
+          :href="reference.href"
+          target="_blank"
+        >
+          <span v-html="reference.href" />
+        </a>
+      </p>
+    </div>
   </div>
 </template>
 <script>
-export default {
-    'name': 'References'
-}
+    import references from "../assets/references/references.js";
+
+    export default {
+        name: "References",
+        data() {
+            return {
+                title: references.referenceSection.title,
+                references: references.referenceSection.references
+            };
+        },
+    }
 </script>
-<style lang="scss" scoped>
+
+<style scoped lang="scss">
 
 </style>
