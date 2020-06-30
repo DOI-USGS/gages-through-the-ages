42a23c2fc92a629e62de93276e58725a6f71ca25
Marty Wernimont <mwernimont87@gmail.com>
2020-06-30 13:08:18 -0500

    New Annotated Mobile Images

--- src/components/AnnotatedTimeline.vue
+++ src/components/AnnotatedTimeline.vue
@@ -4,10 +4,37 @@
     class="section"
   >
     <h2>{{ text.title }}</h2>
-    <img
-      src="@/assets/images/barChart.jpg"
-      alt="annotated timeline showing important periods in gage history"
-    >
+    <div v-if="smallScreen" id="mobileAnnotatedBarCharts" class="breathingRoom">
+      <picture>
+        <source srcset="@/assets/images/annotatedBarChart/mobile/pastEra.webp" type="image/webp">
+        <img
+          src="@/assets/images/annotatedBarChart/mobile/pastEra.jpg"
+          alt="annotated timeline showing the years 1900 to 1930."
+        >
+      </picture>
+      <picture>
+        <source srcset="@/assets/images/annotatedBarChart/mobile/midEra.webp" type="image/webp">
+        <img
+          src="@/assets/images/annotatedBarChart/mobile/midEra.jpg"
+          alt="annotated timeline showing the years 1930 to 1960."
+        >
+      </picture>
+      <picture>
+        <source srcset="@/assets/images/annotatedBarChart/mobile/presentEra.webp" type="image/webp">
+        <img
+          src="@/assets/images/annotatedBarChart/mobile/presentEra.jpg"
+          alt="annotated timeline showing the years 1960 to the present."
+        >
+      </picture>
+    </div>
+    <div v-if="largeScreen" id="annotatedBarChart" class="breathingRoom">
+      <picture>
+        <img
+          src="@/assets/images/annotatedBarChart/barChart.jpg"
+          alt="annotated timeline showing important periods in gage history"
+        >
+      </picture>
+    </div>
     <div
       v-for="paragraph in text.paragraphSections"
       :key="paragraph.paragraphText"
@@ -24,15 +51,43 @@
       name: 'AnnotatedTimeline',
       data() {
           return {
+              smallScreen: false,
+              largeScreen: false,
               text: annotatedTimelineText.textContents
           }
+      },
+      mounted(){
+        let self = this;
+        window.addEventListener('load', function(){
+          let windowSize = window.innerWidth;
+          self.checkWindowSize(windowSize);
+        }, {passive: true})
+        window.addEventListener('resize', function(){
+          console.log('resized');
+          let windowSize = window.innerWidth;
+          self.checkWindowSize(windowSize);
+          console.log(windowSize);
+        }, {passive: true});  
+      },
+      methods:{
+        checkWindowSize(windowSize){
+          if(windowSize <= 650){
+            this.smallScreen = true;
+            this.largeScreen = false;
+          }else{
+            this.smallScreen = false;
+            this.largeScreen = true;
+          }
+        }
       }
   }
-
 </script>
 
 <style scoped lang="scss">
-  img{
+  #mobileAnnotatedBarCharts{
+    text-align: center;
+  }
+  .breathingRoom{
     margin: 4vh 0;
   }
 </style>
