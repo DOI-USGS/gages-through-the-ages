d730c89c6b45e760881370e41e8dd32645651726
Marty Wernimont <mwernimont87@gmail.com>
2020-07-16 12:33:45 -0500

    Add link in Take Away Text

--- src/assets/takeAway/takeAwayText.js
+++ src/assets/takeAway/takeAwayText.js
@@ -5,8 +5,9 @@ export default {
         paragraphSections: [
             {paragraphText: 'As factors that influence water monitoring change, the USGS is continually ' + 
                             'adapting to the present-day needs of water management and the protection of life ' + 
-                            'and property. A new initiative established in 2019, called the "Next-Generation Water ' + 
-                            'Observing System” is piloting new advanced water monitoring in the Delaware River Basin ' + 
+                            'and property. A new initiative established in 2019, called the ' + 
+                            '<a href="https://www.usgs.gov/mission-areas/water-resources/science/usgs-next-generation-water-observing-system-ngwos" target="_blank">Next-Generation Water ' + 
+                            'Observing System</a> is piloting new advanced water monitoring in the Delaware River Basin ' + 
                             'and will be expanding into the Upper Colorado River Basin next year. This new initiative will ' + 
                             'deliver high resolution water data using innovative technologies to support USGS regional assessments ' + 
                             'and models  for water availability assessments and prediction. These new sources of water information ' + 
