140e8721e1db65174c72ee0de0328a1326ee954f
Marty Wernimont <mwernimont87@gmail.com>
2020-07-16 12:29:56 -0500

    Take Away Text Update

--- src/assets/takeAway/takeAwayText.js
+++ src/assets/takeAway/takeAwayText.js
@@ -4,11 +4,14 @@ export default {
         introText: "",
         paragraphSections: [
             {paragraphText: 'As factors that influence water monitoring change, the USGS is continually ' + 
-                            'adapting to the present-day needs of water management and the protection of life ' +
-                            'and property. A new initiative established in 2019, called the "Next-Generation Water Observing System” ' +
-                            'is piloting new advanced water monitoring in the Delaware River Basin and will be expanding into the ' +
-                            'Upper Colorado River Basin next year. These new sources of water information will inform water resources ' + 
-                            'decisions and advance the understanding of how best to meet the nation’s changing water needs.'
+                            'adapting to the present-day needs of water management and the protection of life ' + 
+                            'and property. A new initiative established in 2019, called the "Next-Generation Water ' + 
+                            'Observing System” is piloting new advanced water monitoring in the Delaware River Basin ' + 
+                            'and will be expanding into the Upper Colorado River Basin next year. This new initiative will ' + 
+                            'deliver high resolution water data using innovative technologies to support USGS regional assessments ' + 
+                            'and models  for water availability assessments and prediction. These new sources of water information ' + 
+                            'will inform water resources decisions and advance the understanding of how best to meet the nation’s ' + 
+                            'changing water needs. '
             },
         ]
     }
