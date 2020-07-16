cfacc3840a83aedae1b41628a66758f81c12d6fd
Marty Wernimont <mwernimont87@gmail.com>
2020-07-16 12:18:05 -0500

    Annotated Bar Chart Text Updates

--- src/assets/annotatedTimeline/annotatedTimeline.js
+++ src/assets/annotatedTimeline/annotatedTimeline.js
@@ -1,11 +1,32 @@
 export default {
     textContents: {
-        title: "A deeper look at the timeline",
+        title: "A Brief History of USGS Streamgaging ",
         introText: "",
         paragraphSections: [
-            {paragraphText: 'This is a section of placeholder text related to the annotated bar chart.'
+            {paragraphText: 'USGS streamgaging began in 1889 with the establishment of the first gage in Embudo, NM ' + 
+                            'where a team of hydrographers led by John Wesley Powell were trained in new techniques measure ' +
+                            'water flow systematically. The Embudo gage marked the beginning of an era where streamgaging was ' +
+                            'used to assess the potential of the nation’s water resources for growth in the western US. These ' + 
+                            'objectives defined much of the monitoring priorities for the USGS in the early 1900’s and were aligned ' + 
+                            'with the earliest period of growth in gages, a slow accumulation of monitoring sites that resulted in less ' + 
+                            'than 200 active gages by the turn of the century. This period of water assessment continued until approximately ' + 
+                            '1930, when the need to understand the potential of western waters for hydropower and irrigation began to plateau ' +
+                            'while the need to design smarter flood control structures was on the rise.  '
             },
-            {paragraphText: 'This is an optional second paragraph placeholder for text related to the annotated bar chart.'
+            {paragraphText: 'The period of 1930-1960 was marked by infrastructure investments in response to major floods (some of which occurred ' + 
+                            'in the 1920’s); new dams and release structures were engineered to better protect life and property from future floods ' + 
+                            'and required a detailed understanding of the expected ranges of river flows. In 1929, Congress established the USGS ' + 
+                            'Cooperative Program which supported cost sharing with State and local agencies, resulting in a new source of funding that ' + 
+                            'is responsible for the largest sustained growth during the history of US streamgaging, which continues to present day.  '
+            },
+            {paragraphText: 'Indeed, the modern (current) era of monitoring is defined by a greater number of complex and intertwined local to national-scale ' + 
+                            'water issues that rely on timely information in order to better manage water and provide advanced warning of dangerous water conditions. ' + 
+                            'Many of these priorities have grown and declined (such as the coal hydrology studies of the 1970’s) and are visible as up and down variations ' + 
+                            'in the national gage count. The USGS also increased focus on optimizing the use of national-scale stream monitoring resources during 1991-1996 when ' + 
+                            'stringent budget cuts reduced gage funding; some gages were strategically discontinued in order to support the continued operation of others. Needs '  + 
+                            'for advanced warning of floods has driven much of the growth in gages during the late 1990’s and up until present day. While advanced data availability ' + 
+                            'was in place for a small number of gages in earlier eras, the establishment of public delivery of real-time streamflow information provided timely ' + 
+                            'answers to many new water questions.  '
             },
         ]
     }
