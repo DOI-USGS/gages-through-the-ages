3f07eb689f521b24d45297d7484c69354f358de8
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-07-24 12:06:40 -0500

    lint changes

--- src/components/Methods.vue
+++ src/components/Methods.vue
@@ -3,7 +3,7 @@
     id="methods"
     class="section"
   >
-    <h2><a id="methods-anchor"></a>Methods</h2>
+    <h2><a id="methods-anchor" />Methods</h2>
     <div class="usa-accordion usa-accordion--bordered">
       <div
         v-for="method in methods.methods"
