5789a9d6293304e184ce96a177348a7fee54873c
Cee Nell <cnell@usgs.gov>
2023-05-14 19:36:25 -0700

    dplyr

--- gage_timeseries_gif/src/data_utils.R
+++ gage_timeseries_gif/src/data_utils.R
@@ -5,7 +5,7 @@ time_data <- function(gage_data){
   gage_data |>
     unnest(which_years_active) |>
     unnest(gap_years) |>
-    select(-n_years_active, -earliest_active_year) |>
+    dplyr::select(-n_years_active, -earliest_active_year) |>
     transform(gap_years = as.numeric(gap_years)) |>
     pivot_longer(c(which_years_active, gap_years), values_to = 'year', names_to = 'activity') |>
     filter(!is.na(year), year >= 1889, activity == 'which_years_active')
