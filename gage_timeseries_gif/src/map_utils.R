6a940f37613e65ddd51de5a2e6065f7e0ebc9029
Cee Nell <cnell@usgs.gov>
2023-05-16 17:53:26 -0700

    call libs

--- gage_timeseries_gif/src/map_utils.R
+++ gage_timeseries_gif/src/map_utils.R
@@ -5,8 +5,6 @@
 
 proj.string <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
 to_sp <- function(...){
-  library(maptools)
-  library(maps)
   map <- maps::map(..., fill = TRUE, plot = FALSE)
   IDs <- sapply(strsplit(map$names, ":"), function(x) x[1])
   map.sp <- map2SpatialPolygons(map, IDs=IDs, proj4string=CRS("+proj=longlat +datum=WGS84"))
@@ -16,7 +14,6 @@ to_sp <- function(...){
 
 #' @param locations a data.frame with dec_long_va and dec_lat_va
 points_sp <- function(locations){
-  library(dplyr)
   points <- cbind(locations$dec_long_va, locations$dec_lat_va) 
   points_sp <- sp::SpatialPoints(points, CRS("+proj=longlat +datum=WGS84")) 
   points_transform <- sp::spTransform(points_sp, proj.string) %>% 
