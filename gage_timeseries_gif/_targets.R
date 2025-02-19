07bf34729a215711eb3220cb47c8dd9acaff2b3c
Althea A Archer <aaarcher@usgs.gov>
2025-02-19 12:26:31 -0600

    gif works

--- gage_timeseries_gif/_targets.R
+++ gage_timeseries_gif/_targets.R
@@ -17,6 +17,9 @@ sysfonts::font_add_google(font_fam, regular.wt = 300, bold.wt = 700)
 showtext::showtext_opts(dpi = 300)
 showtext::showtext_auto(enable = TRUE)
 
+# years in time series 
+years_to_plot <- seq(1889, 2024, by = 1)
+
 proj.string <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
 
 list(
@@ -70,13 +73,15 @@ list(
     extract_sites(area_name = "PR", gage_info = gage_info)
   ),
   tarchetypes::tar_map(
-    values = list(active_year = seq(2020, 2024, by = 1)),#seq(1889, 2024, by = 1)
+    # Years 
+    values = list(active_year = years_to_plot),
     # Plot bar chart of active streamgages through time
     tar_target(gage_bar_list,
                plot_gage_timeseries(gage_melt, 
                                     yr = active_year, 
                                     font_fam,
                                     max_year = 2025)),
+    # Map active streamgages through time - for each region
     tar_target(
       gage_map_list_CONUS,
       plot_gage_map(gage_melt = gage_melt, 
@@ -101,6 +106,7 @@ list(
                     yr = year_list, 
                     site_map = site_map_HI, 
                     state_map = state_map_HI)),
+    # Plot bar chart and map together 
     tar_target(
       gage_frames,
       compose_chart(bar_chart = gage_bar_list, 
@@ -113,78 +119,30 @@ list(
     unlist = TRUE,
     names = active_year
   ),
+
+  # Years for animation
+  tar_target(
+    year_list,
+    years_to_plot
+  ),
+  
+  # Intermediate target to make sure the files have been updated before
+  # running the full animation target
+  tarchetypes::tar_files(
+    gage_png_files,
+    sprintf("out/gage_time_%s.png", year_list)
+  ),
+  
+  # Create animation 
   tar_target(
     gage_gif,
-    animate_frames_gif(frames = gage_frames, 
+    animate_frames_gif(frames = gage_png_files, 
                        out_file = 'out/gage_timeseries.gif',
                        reduce = TRUE, 
                        frame_delay_cs = 20, 
                        frame_rate = 4),
     format = 'file'
   ),
-  # Years for animation
-  tar_target(
-    year_list,
-    #seq(1889, 2024, by = 1)
-    seq(2020, 2024, by = 1)
-  ),
-  # Plot bar chart of active streamgages through time
-  # tar_target(
-  #   gage_bar_list,
-  #   plot_gage_timeseries(gage_melt, 
-  #                        year_list, 
-  #                        font_fam,
-  #                        max_year = 2025),
-  #   pattern = map(year_list)
-  # ),
-  # Map active streamgages through time - shifted layout
-  # tar_target(
-  #   gage_map_list_CONUS,
-  #   plot_gage_map(gage_melt = gage_melt, 
-  #                 active_year = year_list, 
-  #                 site_map = site_map_CONUS, 
-  #                 state_map = state_map_CONUS),
-  #   pattern = map(year_list)
-  # ),
-  # tar_target(
-  #   gage_map_list_AK,
-  #   plot_gage_map(gage_melt = gage_melt, 
-  #                 active_year = year_list, 
-  #                 site_map = site_map_AK, 
-  #                 state_map = state_map_AK),
-  #   pattern = map(year_list)
-  # ),
-  # tar_target(
-  #   gage_map_list_PR,
-  #   plot_gage_map(gage_melt = gage_melt, 
-  #                 active_year = year_list, 
-  #                 site_map = site_map_PR, 
-  #                 state_map = state_map_PR),
-  #   pattern = map(year_list)
-  # ),
-  # tar_target(
-  #   gage_map_list_HI,
-  #   plot_gage_map(gage_melt = gage_melt, 
-  #                 active_year = year_list, 
-  #                 site_map = site_map_HI, 
-  #                 state_map = state_map_HI),
-  #   pattern = map(year_list)
-  # ),
-  # Plot bar chart and map together 
-
-  #  pattern = map(year_list, gage_bar_list, gage_map_list_CONUS)
-  #),
-  #tar_target(
-  #  gage_frames_scaled,
-  #  resize_frames(gage_frames, scale_width = 800, dir_out = 'out/'),
-  #  format = 'file'
-  #),
-  # Animate frames to gif
-  # tarchetypes::tar_combine(
-  #   name = gage_gif,
-  #   command = animate_frames_gif(gage_frames, 'out/gage_timeseries.gif',
-  #                                reduce = TRUE, frame_delay_cs = 20, frame_rate = 4)
-  # ),
 
   # Add logo to gif
   tar_target(
