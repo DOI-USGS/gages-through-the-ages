807cbae23cc148117b78d29c74e7c7c2997d2541
Corson-Dosch <hcorson-dosch@usgs.gov>
2025-02-25 07:21:07 -0600

    Make state gif for MT

--- gage_timeseries_gif/_targets.R
+++ gage_timeseries_gif/_targets.R
@@ -2,9 +2,9 @@ library(targets)
 library(tidyverse)
 
 options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("tidyverse", "raster", "sf", 
+tar_option_set(packages = c("tidyverse", "raster", "sf",
                             "maps", "maptools",
-                            "scico", "cowplot", "geomtextpath", 
+                            "scico", "cowplot", "geomtextpath",
                             "sp","magick"))
 
 source('src/data_utils.R')
@@ -18,6 +18,7 @@ showtext::showtext_opts(dpi = 300)
 showtext::showtext_auto(enable = TRUE)
 
 proj.string <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
+proj.string.MT <- "+proj=aea +lat_0=46.9 +lon_0=-109.6 +lat_1=45.8 +lat_2=47.6 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
 
 list(
   # Output of `national-flow-observations`
@@ -25,45 +26,88 @@ list(
     gage_data,
     readRDS("data/active_flow_gages_summary_wy.rds")
   ),
+  tar_target(
+    gages_info,
+    gage_data %>%
+      pull(site) %>%
+      dataRetrieval::readNWISsite()
+  ),
+  tar_target(
+    gages_info_MT,
+    gages_info |>
+      filter(state_cd == '30')
+  ),
   # Put in long form with site_no x years active
   tar_target(
     gage_melt,
-    time_data(gage_data) 
+    time_data(gage_data)
+  ),
+  tar_target(
+    gage_melt_MT,
+    gage_melt |>
+      filter(site %in% pull(gages_info_MT, site_no))
   ),
   # Prep shifted states for map layout
   tar_target(
     AK,
-    list(abrv = 'AK', scale = 0.37, shift = I(c(90,-460)), 
+    list(abrv = 'AK', scale = 0.37, shift = I(c(90,-460)),
          rotate = I(-50), regions = I("USA:alaska"))
   ),
   tar_target(
     HI,
-    list(abrv ='HI', scale = 1, shift = I(c(520, -110)), 
+    list(abrv ='HI', scale = 1, shift = I(c(520, -110)),
          rotate = I(-35), regions = I("USA:hawaii"))
   ),
   tar_target(
-    PR, 
-    list(abrv = 'PR', scale = 2.5, shift = I(c(-140, 90)), 
+    PR,
+    list(abrv = 'PR', scale = 2.5, shift = I(c(-140, 90)),
          rotate = 20, regions = I("Puerto Rico"))
   ),
   tar_target(
     state_map,
     fetch_state_map(AK, HI, PR)
   ),
+  tar_target(
+    state_map_MT,
+    tigris::states(cb = TRUE) %>%
+      dplyr::filter(NAME == 'Montana') %>%
+      sf::st_transform(crs = proj.string.MT)
+  ),
+  tar_target(
+    county_map_MT,
+    tigris::counties(cb = TRUE, state = 'Montana') %>%
+      sf::st_transform(crs = proj.string.MT)
+  ),
   # Prep shifted sites for map layout
   tar_target(
     site_map,
     shift_sites(AK, HI, PR, gage_data = "data/active_flow_gages_summary_wy.rds")
   ),
+  tar_target(
+    site_map_MT,
+    gages_info_MT %>%
+      points_sp()
+  ),
   # Years for animation
   tar_target(
     year_list,
-    seq(1889, 2021, by = 1)
+    seq(1889, 2022, by = 1)
   ),
   # Plot bar chart of active streamgages through time
   tar_target(
     gage_bar_list,
-    plot_gage_timeseries(gage_melt, year_list, font_fam),
+    plot_gage_timeseries(gage_melt, year_list, font_fam,
+                         break_value = 2000,
+                         y_expansion_value = 500,
+                         text_smoothing_value = 30),
+    pattern = map(year_list)
+  ),
+  tar_target(
+    gage_bar_list_MT,
+    plot_gage_timeseries(gage_melt_MT, year_list, font_fam,
+                         break_value = 50,
+                         y_expansion_value = 10,
+                         text_smoothing_value = 45),
     pattern = map(year_list)
   ),
   # Map active streamgages through time - shifted layout
@@ -72,12 +116,26 @@ list(
     plot_gage_map(gage_melt, year_list, site_map, state_map),
     pattern = map(year_list)
   ),
-  # Plot bar chart and map together 
+  tar_target(
+    gage_map_list_MT,
+    plot_gage_map_state(gage_melt_MT, year_list, site_map_MT,
+                        state_map_MT, county_map_MT),
+    pattern = map(year_list)
+  ),
+  # Plot bar chart and map together
   tar_target(
     gage_frames,
-    compose_chart(gage_bar_list, gage_map_list, year = year_list),
+    compose_chart(gage_bar_list, gage_map_list, year = year_list,
+                  outfile_template = 'out/gage_time_%s.png'),
     format = 'file',
     pattern = map(year_list, gage_bar_list, gage_map_list)
+  ),
+  tar_target(
+    gage_frames_MT,
+    compose_chart(gage_bar_list_MT, gage_map_list_MT, year = year_list,
+                  outfile_template = 'out/gage_time_%s_MT.png'),
+    format = 'file',
+    pattern = map(year_list, gage_bar_list_MT, gage_map_list_MT)
   ),
   #tar_target(
   #  gage_frames_scaled,
@@ -91,6 +149,12 @@ list(
                    reduce = TRUE, frame_delay_cs = 20, frame_rate = 4),
     format = 'file'
   ),
+  tar_target(
+    gage_gif_MT,
+    animate_frames_gif(gage_frames_MT, 'out/gage_timeseries_MT.gif',
+                       reduce = TRUE, frame_delay_cs = 20, frame_rate = 4),
+    format = 'file'
+  ),
   # Add logo to gif
   tar_target(
     gif_final,
@@ -99,10 +163,24 @@ list(
       usgs_logo <- magick::image_read('usgs_logo_grey.png') |>
         magick::image_resize('x80') |>
         magick::image_colorize(100, "grey80")
-     # add to gif 
+     # add to gif
      image_read(gage_gif) |>
      image_composite(usgs_logo, offset = '+180+920') |>
       image_write('out/gage_timeseries_logo.gif')
+    },
+    format = 'file'
+  ),
+  tar_target(
+    gif_final_MT,
+    {
+      # import logo
+      usgs_logo <- magick::image_read('usgs_logo_grey.png') |>
+        magick::image_resize('x80') |>
+        magick::image_colorize(100, "grey80")
+      # add to gif
+      image_read(gage_gif_MT) |>
+        image_composite(usgs_logo, offset = '+180+920') |>
+        image_write('out/gage_timeseries_MT_logo.gif')
     },
     format = 'file'
   )
--- gage_timeseries_gif/src/plot_utils.R
+++ gage_timeseries_gif/src/plot_utils.R
@@ -3,42 +3,44 @@
 #' Plot bar chart of active gages
 #' @param gage_melt long form, gage site_no & years active
 #' @param yr year being shown
-plot_gage_timeseries <- function(gage_melt, yr, font_fam){
-  
+plot_gage_timeseries <- function(gage_melt, yr, font_fam, break_value,
+                                 y_expansion_value, text_smoothing_value){
+
   # store vars for plotting
   active_year <- as.numeric(yr)
   yr_max <- max(gage_melt$year)
-  
+
   gages_by_year <- gage_melt |>
     group_by(year) |>
     summarize(n_sites = length(unique(site)))
-  
+
   ## plot bar chart
   active_year <- yr
-  
+
   gages_by_year |>
     ggplot(
       aes(year, n_sites)
     ) +
     geom_bar(stat = "identity", fill = "#ededed", width = 0.8) +
-    geom_bar(data = .%>% filter(year == active_year), 
+    geom_bar(data = .%>% filter(year == active_year),
              stat = "identity",
              fill = '#0962b2') +
-    geom_textline(data = gages_by_year, 
-                  aes(x = year, y = n_sites, 
+    geom_textline(data = gages_by_year,
+                  aes(x = year, y = n_sites,
                       label = "Number of active gages through time"),
                   hjust = 0.05,
-                  color = 'grey70', size = 8/.pt, vjust = -0.35, text_smoothing = 30,
+                  color = 'grey70', size = 8/.pt, vjust = -0.35,
+                  text_smoothing = text_smoothing_value,
                   family = font_fam, linecolor = NA
     ) +
     scale_x_continuous(
-      breaks = scales::breaks_width(10), 
+      breaks = scales::breaks_width(10),
       expand = c(0,0.025),
-      limits = c(NA, 2022)
+      limits = c(NA, yr_max)
     ) +
     scale_y_continuous(
-      breaks = scales::breaks_width(2000),
-      expand = expansion(add = c(500,0))
+      breaks = scales::breaks_width(break_value),
+      expand = expansion(add = c(y_expansion_value,0))
     ) +
     ylab(NULL) + xlab(NULL) +
     theme(
@@ -50,7 +52,7 @@ plot_gage_timeseries <- function(gage_melt, yr, font_fam){
       axis.text.y = element_text(hjust = 1, margin=margin(r = -0.5, t = 0)),
       axis.text.x = element_text(vjust = 0, margin=margin(r = 0, t = -2))
     )
-  
+
 }
 
 #' Plot map of active gages
@@ -59,46 +61,78 @@ plot_gage_timeseries <- function(gage_melt, yr, font_fam){
 #' @param site_map shifted sites to matcvh state_map
 #' @param state_map shifted states and territories
 plot_gage_map <- function(gage_melt, active_year, site_map, state_map){
-  
+
   # filter gage data to given year
   yr_gages <- gage_melt |>
     filter(year == as.numeric(active_year)) |>
     distinct(site)
-  
-  # convert to sf 
+
+  # convert to sf
   states.sf <- state_map |> st_as_sf()
-  sites.sf <- site_map |> st_as_sf() |> 
-    distinct() |> 
+  sites.sf <- site_map |> st_as_sf() |>
+    distinct() |>
     st_transform(st_crs(states.sf)) |>
     st_intersection(states.sf) # drops south pacific islands
-                                  
+
   # sites active in a given year
   gages_active <- sites.sf |>
     filter(site_no %in% yr_gages$site)
-  
+
   # plot map
   gage_map <- gages_active |>
     ggplot() +
     geom_sf(data = states.sf, fill = '#ededed',
             linewidth = 0.25, color = 'white') +
     geom_sf(color = '#0962b2', size = 0.1, alpha = 0.7) +
-    theme_void() 
-  
+    theme_void()
+
   return(gage_map)
-  
+
 }
 
+#' @param state_map shifted states and territories
+plot_gage_map_state <- function(gage_melt, active_year, site_map, state_map,
+                                county_map){
+
+  # filter gage data to given year
+  yr_gages <- gage_melt |>
+    filter(year == as.numeric(active_year)) |>
+    distinct(site)
+
+  # sites
+  sites.sf <- site_map |> st_as_sf() |>
+    distinct() |>
+    st_transform(st_crs(state_map)) |>
+    st_intersection(state_map) # not sure this is needed
+
+  # sites active in a given year
+  gages_active <- sites.sf |>
+    filter(site_no %in% yr_gages$site)
+
+  # plot map
+  gage_map <- gages_active |>
+    ggplot() +
+    geom_sf(data = state_map, fill = '#ededed',
+            linewidth = 0.25, color = 'white') +
+    geom_sf(data = county_map, fill = NA,
+            linewidth = 0.1, color = 'white') +
+    geom_sf(color = '#0962b2', size = 0.1, alpha = 0.7) +
+    theme_void()
+
+  return(gage_map)
+
+}
 
 #' Compose map and bar chart together
 #' @param bar_chart timeseries bar chart of active flow gages
 #' @param year year being shown
 #' @param gage_map map of active gages
-compose_chart <- function(bar_chart, gage_map, year){
-  
+compose_chart <- function(bar_chart, gage_map, year, outfile_template){
+
   ggdraw(xlim = c(0, 1), ylim = c(0,1)) +
     # create background canvas
     draw_grob(grid::rectGrob(
-      x = 0, y = 0, 
+      x = 0, y = 0,
       width = 2, height = 2,
       gp = grid::gpar(fill = 'white', alpha = 1, col = 'white')
     )) +
@@ -114,9 +148,9 @@ compose_chart <- function(bar_chart, gage_map, year){
       y = 0,
       height = 0.25, width = 1 - 0.1
     )
-  
-  ggsave(sprintf('out/gage_time_%s.png', year), width = 5, height = 4, dpi = 300, units = 'in')
-  
+
+  ggsave(sprintf(outfile_template, year), width = 5, height = 4, dpi = 300, units = 'in')
+
 }
 
 #' Animate frames to create gif
@@ -124,7 +158,7 @@ compose_chart <- function(bar_chart, gage_map, year){
 #' @param out_file output file name for gif
 #' @param frame_delay_cs time spent on each frame
 #' @param reduce logical, whether or not to apply gifsicle compression
-#' @param frame_rate frames per second 
+#' @param frame_rate frames per second
 animate_frames_gif <- function(frames, out_file, reduce = TRUE, frame_delay_cs, frame_rate){
   frames %>%
     image_read() %>%
@@ -135,43 +169,43 @@ animate_frames_gif <- function(frames, out_file, reduce = TRUE, frame_delay_cs, 
       fps = frame_rate
     ) %>%
     image_write(out_file)
-  
+
   if(reduce == TRUE){
     optimize_gif(out_file, frame_delay_cs)
   }
-  
+
   return(out_file)
-  
+
 }
 
 #' Resize frames
 #' @param frames list of pngs, frames for gif
 #' @param scale_width desired output frame width
 #' @param scale_percent optional, resize frames using percent of current size
-#' @param dir_out Directory to store resized frames 
+#' @param dir_out Directory to store resized frames
 resize_frames <- function(frames, scale_width, scale_percent = NULL, dir_out) {
-  
+
   if(!dir.exists(dir_out)) dir.create(dir_out)
-  
+
   frames_in <- frames %>%
     image_read() %>%
     image_join()
   info <- image_info(frames_in)
-  
-  # scale png frames proportionally 
+
+  # scale png frames proportionally
   # either based on a percentage of current size or width
   if(!is.null(scale_percent)){
     scale_width <- info$width*(scale_percent/100)
   }
-  
+
   frames_scaled <- frames_in %>%
-    image_scale(scale_width) 
-  
-  png_names <-  sprintf('%s/%s', dir_out, gsub('out/', '', frames)) 
+    image_scale(scale_width)
+
+  png_names <-  sprintf('%s/%s', dir_out, gsub('out/', '', frames))
   map2(as.list(frames_scaled), png_names, ~image_write(.x, .y))
-  
+
   return(png_names)
-  
+
 }
 
 
@@ -179,12 +213,12 @@ resize_frames <- function(frames, scale_width, scale_percent = NULL, dir_out) {
 #' @param out_file output file name for gif
 #' @param frame_delay_cs time spent on each frame
 optimize_gif <- function(out_file, frame_delay_cs) {
-  
+
   # simplify the gif with gifsicle - cuts size by about 2/3
   gifsicle_command <- sprintf('gifsicle -b -O3 -d %s --colors 20 %s',
                               frame_delay_cs, out_file)
   system(gifsicle_command)
-  
+
   return(out_file)
 }
 
