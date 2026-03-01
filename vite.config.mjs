5c05484f529e5a92f280b6472c34d7dcc372d04d
Cee <cnell@usgs.gov>
2026-03-01 13:48:06 -0600

    clean up unused deps

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
-// import replace from "@rollup/plugin-replace";
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import {ViteEjsPlugin} from "vite-plugin-ejs";
